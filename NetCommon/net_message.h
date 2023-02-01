f979f3f88608f379389e48f9e42a47268925b9a9
Max Tolliday <maxtolliday@gmail.com>
2023-02-02 09:59:28 +1100

    Added NetClient and more headers. Finished part 1 of javidx9's guide

--- NetCommon/net_message.h
+++ NetCommon/net_message.h
@@ -23,24 +23,60 @@ namespace olc
 			// Returns size of entire message packet in bytes
 			size_t size() const
 			{
-				return body.size();
+				return sizeof(message_header<T>) + body.size();
 			}
 
-			//Override for std::out compatability - produces friendly description of message
+			// Override for std::out compatability - produces friendly description of message
 			friend std::ostream& operator << (std::ostream& os, const message<T>& msg)
 			{
 				os << "ID:" << int(msg.header.id) << " Size:" << msg.header.size;
 				return os;
 			}
 
-			//Pushes any POD-like data into the message buffer
+			// Pushes any POD-like data into the message buffer
 			template<typename DataType>
 			friend message<T>& operator << (message<T>& msg, const DataType& data)
 			{
-				//Check that the type of data being pushed is trivially copyable
-				static_assert(std::is)
+				// Check that the type of data being pushed is trivially copyable
+				static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pushed into the vector");
+
+				// Cache currect size of vector, as this will be the point we insert the data
+				size_t i = msg.body.size();
+
+				// Resize the vector by the size of the data being pushed
+				msg.body.resize(msg.body.size() + sizeof(DataType));
+
+				// Physically copy the data into the newly allocated vetor space
+				std::memcpy(msg.body.data() + i, &data, sizeof(DataType));
+
+				// Recalculate the message size
+				msg.header.size = msg.size();
+
+				// Return the target message so it can be "chained"
+				return msg;
 			}
 
 		};
+
+		// Forward declear the connection
+		template <typename T>
+		class connection;
+
+		template <typename T>
+		struct owned_message
+		{
+			std::shared_ptr<connection<T>> remote = nullptr;
+			message<T> msg;
+
+			// Again, a friendly string maker
+			friend std::ostream& operator<<(std::ostream& os, const owned_message<T>& msg)
+			{
+				os << msg.msg;
+				return os;
+			}
+		};
+
+
+
 	}
 }
